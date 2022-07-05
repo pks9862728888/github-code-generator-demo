c281d4aa692fef3a182ad8586b50f7831f65082d
hackedlings <pks9862728888@gmail.com>
2022-07-05 08:38:29 +0530

    First commit

---
+++ src/main/java/com/example/githubcodegeneratordemo/GithubCodeGeneratorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubcodegeneratordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCodeGeneratorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCodeGeneratorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubcodegeneratordemo/GithubCodeGeneratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubcodegeneratordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCodeGeneratorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
